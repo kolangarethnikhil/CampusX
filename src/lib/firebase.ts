7e2b7312f28eb47ad46d69fc345239fa27953ec2
kolangareth nikhil <nikhilnandhu4@gmail.com>
2026-05-08 19:04:38 +0530

    use vercel env for firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvQE6ZypCKa_N0xo2i130yLTm5hLM47pw",
-  authDomain: "kju1-a25b4.firebaseapp.com",
-  projectId: "kju1-a25b4",
-  storageBucket: "kju1-a25b4.firebasestorage.app",
-  messagingSenderId: "109020994202",
-  appId: "1:109020994202:web:e1f04daf7799578a575d23",
-  measurementId: "G-7EBMGFHQ5M",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
